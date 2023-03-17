fe108e9d542467489cf8a05478ef93d244688129
Parth Patel <parthvnp@gmail.com>
2023-03-16 22:02:40 -0400

    Add 804[py]

---
+++ src/main/python/hash_table/morse_unique_words.py
@@ -0,0 +1,23 @@
+from typing import *
+
+
+class UniqueMorseCode:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                      "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+        def build_code(word):
+            s = ""
+            for i in word: s += morse_code[ord(i) - 97]
+            return s
+
+        def count_transformations(words):
+            print([build_code(w) for w in words])
+            return len(set([build_code(w) for w in words]))
+
+        return count_transformations(words)
+
+if __name__ == "__main__":
+    s = UniqueMorseCode()
+    r = s.uniqueMorseRepresentations(["a"])
+    print(r)
