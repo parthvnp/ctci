4e7a15f0bff480b0e732a37c910030e90edfa55f
Parth Patel <parthvnp@gmail.com>
2023-04-01 22:32:52 -0400

    Add LC-2300 [Python]

---
+++ src/main/python/binary_search/successful_pairs.py
@@ -0,0 +1,7 @@
+from bisect import bisect_left
+
+
+class SuccessfulPairs:
+    def successfulPairs(self, spells, potions, success):
+        potions.sort()
+        return [len(potions) - bisect_left(potions, (success + a - 1) // a) for a in spells]
