3bee6bbb77f5213a68ec1f0082d063122d20f899
Parth Patel <parthvnp@gmail.com>
2023-04-18 23:00:28 -0400

    Add LC-1372 [Python]

---
+++ src/main/python/tree/max_zig_zag_path.py
@@ -0,0 +1,38 @@
+from tree_lib import TreeNode, build_tree
+from typing import Optional
+
+
+class MaxZigZagPath:
+    def __init__(self):
+        self.pathLength = None
+
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.pathLength = 0
+
+        def dfs(node, goLeft, steps):
+            if node:
+                self.pathLength = max(self.pathLength, steps)
+                if goLeft:
+                    dfs(node.left, False, steps + 1)
+                    dfs(node.right, True, 1)
+                else:
+                    dfs(node.left, False, 1)
+                    dfs(node.right, True, steps + 1)
+
+        dfs(root, False, 0)
+        dfs(root, True, 0)
+        return self.pathLength
+
+
+if __name__ == "__main__":
+    s = MaxZigZagPath()
+    e1 = s.longestZigZag(
+        TreeNode(1, TreeNode(1, TreeNode(1), TreeNode(1)), TreeNode(1, None, TreeNode(1, TreeNode(1), TreeNode(1))))
+    )
+    print(e1 == 2, e1)
+    e2 = s.longestZigZag(TreeNode(1, TreeNode(1, TreeNode(1), TreeNode(1, TreeNode(1), TreeNode(1))), TreeNode(1)))
+    print(e2 == 3, e2)
+    e3 = s.longestZigZag(TreeNode(1))
+    print(e3 == 0, e3)
+    e4 = s.longestZigZag(build_tree([1, 1, None]))
+    print(e4 == 1, e4)
--- src/main/python/tree/tree_lib.py
+++ src/main/python/tree/tree_lib.py
@@ -4,8 +4,28 @@ def print_inorder(root):
         print(root.val)
         print_inorder(root.right)
 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+def build_tree(traversal) -> TreeNode:
+    if not traversal:
+        return None
+    root = TreeNode(traversal[0])
+    queue = [root]
+    i = 1
+    while i < len(traversal):
+        node = queue.pop(0)
+        if traversal[i] is not None:
+            node.left = TreeNode(traversal[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(traversal) and traversal[i] is not None:
+            node.right = TreeNode(traversal[i])
+            queue.append(node.right)
+        i += 1
+    return root
