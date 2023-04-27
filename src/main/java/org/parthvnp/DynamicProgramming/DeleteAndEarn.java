c012f4f7aa4abd3f1a7800acf42422f169ecd2e8
Parth Patel <parthvnp@gmail.com>
2023-04-27 00:38:28 -0400

    Add LC-740 [Java]

--- src/main/java/org/parthvnp/DynamicProgramming/DeleteAndEarn.java
+++ src/main/java/org/parthvnp/DynamicProgramming/DeleteAndEarn.java
@@ -1,2 +1,25 @@
-package org.parthvnp.DynamicProgramming;public class DeleteAndEarn {
+package org.parthvnp.DynamicProgramming;
+
+public class DeleteAndEarn {
+    public int deleteAndEarn(int[] nums) {
+        int n = 10001;
+        int[] values = new int[n];
+        for (int num : nums) values[num] += num;
+        int[] dp = new int[n];
+        dp[1] = values[1];
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.max(dp[i-2] + values[i], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+
+    public static void main(String[] args) {
+        var s = new DeleteAndEarn();
+        var e1 = s.deleteAndEarn(new int[]{3, 4, 2});
+        System.out.println(e1 == 6);
+        var e2 = s.deleteAndEarn(new int[]{2, 2, 3, 3, 3, 4});
+        System.out.println(e2 == 9);
+        var e3 = s.deleteAndEarn(new int[]{1, 1, 1, 2, 4, 5, 5, 5, 6});
+        System.out.println(e3 == 18);
+    }
 }
