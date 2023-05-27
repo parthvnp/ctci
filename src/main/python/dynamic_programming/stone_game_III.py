f4eb8750b19227138f5693303de8d3d42bf57149
Parth Patel <parthvnp@gmail.com>
2023-05-27 10:23:49 -0400

    Add LC-1406 [Python]

---
+++ src/main/python/dynamic_programming/stone_game_III.py
@@ -0,0 +1,28 @@
+from typing import List
+from shared.test import test
+
+class StoneGameIII:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+
+        def f(i):
+            if i == n:
+                return 0
+            result = stoneValue[i] - f(i + 1)
+            if i + 2 <= n:
+                result = max(result, stoneValue[i] + stoneValue[i + 1] - f(i + 2))
+            if i + 3 <= n:
+                result = max(result, stoneValue[i] + stoneValue[i + 1]
+                            + stoneValue[i + 2] - f(i + 3))
+            return result
+
+        dif = f(0)
+        if dif > 0:
+            return "Alice"
+        if dif < 0:
+            return "Bob"
+        return "Tie"
+
+if __name__ == '__main__':
+    s = StoneGameIII()
+    test("Bob", s.stoneGameIII([1, 2, 3, 7]))
